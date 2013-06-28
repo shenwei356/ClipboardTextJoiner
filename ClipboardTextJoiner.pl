08546cffd5f5f6f0c0876a7ed56fee914f8838d8
shenwei356 <shenwei356@gmail.com>
2013-06-29 00:13:37 +0800

    add a universal edition

--- ClipboardTextJoiner.pl
+++ ClipboardTextJoiner.pl
@@ -1,11 +1,6 @@
 #!/usr/bin/perl;
 use strict;
-use Win32::Clipboard;
-
-my $clip               = Win32::Clipboard();
-my $text               = '';
-my $handle_this_change = 1;
-my $head               = ("-" x 31). "[ Clipboard Text ]". ("-" x 30);
+use Clipboard;
 
 print <<INFO;
 ===============================================================================
@@ -18,23 +13,19 @@ print <<INFO;
 
 INFO
 
+my $head      = ("-" x 31). "[ Clipboard Text ]". ("-" x 30);
+my $text      = '';
+my $last_text = '';
+
 while (1) {
-    if (
-        $clip->WaitForChange()     # clipboard changed
-        and $handle_this_change    # handle this change
-      )
-    {
-        next unless $clip->IsText();    # only text will be edited
-        $text = $clip->GetText();       # get text from clipboard
-        $text = &edit_text($text);      # edit text
-        $handle_this_change = 0;        # to ignore this change
-        $clip->Set($text);              # write back to clipboard
-    }
-    else {
-        $text = $clip->GetText();       # show edited text
+    $text = Clipboard->paste;
+    if ( $text ne $last_text ) {
+        $text = &edit_text($text);
+        Clipboard->copy($text);
         print "\n$head\n$text\n";
-        $handle_this_change = 1;        # handle next change
+        $last_text = $text;
     }
+    sleep 1;
 }
 
 sub edit_text {
